880a24895c5f58ea63da61f2e388f482841e324a
harishac015 <76650457+harishac015@users.noreply.github.com>
2021-01-05 14:00:10 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -5,13 +5,13 @@ pali(){
     char string1[20];
     int i, length;
     int flag = 0;
-    
-    <!--Demo on Githubhooks-->
-    
+       
     printf("\n\nEnter a string:");
     scanf("%s", string1);
     
     length = strlen(string1);
+    
+    <!--Demo on Githubhooks-->
     
     for(i=0;i < length ;i++){
         if(string1[i] != string1[length-i-1]){
